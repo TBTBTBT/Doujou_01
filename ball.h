d543ce4ebc87b27fa25d6051f6e7adedfbc9d798
TBTBTBT <diacute.205@gmail.com>
2016-08-29 10:02:20 +0900

    Create ball.h

---
+++ ball.h
@@ -0,0 +1,13 @@
+#define BALL 10
+class BallSan {
+public:
+    ofVec2f pos;
+    ofVec2f acc;
+    float size;
+    float scale;
+    void junbi(float,float);
+    void ugokasu();
+    void juuryoku(float,float);
+    void hyouji();
+    void ataru(float,float,float);
+};
